57ccab7de4561e45dbf579326d1e7ab44e101e69
jdemas <Jerry.DeMas@marcumllp.com>
2023-11-13 07:58:08 -0500

    Module Updates

--- src/app/products/product-list.component.ts
+++ src/app/products/product-list.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from "@angular/core";
 import { IProduct } from "./product";
+import { ProductService } from "./product.service";
 
 @Component({
   selector: 'pm-products',
@@ -7,12 +8,15 @@ import { IProduct } from "./product";
   styleUrls: ['./product-list.component.css']
 })
 export class ProductListComponent implements OnInit {
+
   pageTitle: string = 'Product List';
   imageWidth: number = 50;
   imageMargin: number = 2;
   showImage: boolean = false;
 
   private _listFilter: string = '';
+
+
   get listFilter(): string {
     return this._listFilter;
   }
@@ -23,28 +27,25 @@ export class ProductListComponent implements OnInit {
   }
 
   filteredProducts: IProduct[] = [];
-  products: IProduct[] = [
-    {
-      "productId": 2,
-      "productName": "Garden Cart",
-      "productCode": "GDN-0023",
-      "releaseDate": "March 18, 2021",
-      "description": "15 gallon capacity rolling garden cart",
-      "price": 32.99,
-      "starRating": 4.2,
-      "imageUrl": "assets/images/garden_cart.png"
-    },
-    {
-      "productId": 5,
-      "productName": "Hammer",
-      "productCode": "TBX-0048",
-      "releaseDate": "May 21, 2021",
-      "description": "Curved claw steel hammer",
-      "price": 8.9,
-      "starRating": 4.8,
-      "imageUrl": "assets/images/hammer.png"
-    }
-  ];
+  products: IProduct[] = [];
+
+
+
+
+  
+
+  //original
+  //private _productService;
+  //constructor(productService: ProductService) {
+  //  this._productService = productService;
+  //}
+
+  //New shorthand way
+  constructor(private productService: ProductService) {
+  }
+
+
+
 
   performFilter(filterBy: string): IProduct[] {
     filterBy = filterBy.toLocaleLowerCase();
@@ -57,7 +58,8 @@ export class ProductListComponent implements OnInit {
   }
 
   ngOnInit(): void {
-    this.listFilter = 'cart';
+    this.products = this.productService.getProducts();
+    this.filteredProducts = this.products;    
   }
 
   onRatingClicked(message: string) : void {
